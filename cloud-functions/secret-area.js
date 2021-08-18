f509f95f5a72d676fbb02ca830f44279ceede8ad
Nick Nie <ersigui@gmail.com>
2021-08-18 16:44:19 +0800

    Update secret-area.js
    add functions to the cloud and check the secret area

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,27 @@
 exports.handler = function(event, context, callback){
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p> I can tell you the reall question <strong>Q.1</strong></p>
+    `
+    
+    let body
+    
+    if (event.body){
+        body = JASON.parse(evetn.body)
+    } else{
+        body = {}
+    }
+
+    if (body.password == 'javascript'){
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+
+    
 }
